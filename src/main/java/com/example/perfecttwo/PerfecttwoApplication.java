4d898db6b6da4048fb010630079ecca0079d14f0
jhg097 <jhg097@naver.com>
2022-03-10 21:53:26 +0900

    * 환경설정

---
+++ src/main/java/com/example/perfecttwo/PerfecttwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.perfecttwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfecttwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfecttwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/perfecttwo/PerfecttwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.perfecttwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfecttwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
